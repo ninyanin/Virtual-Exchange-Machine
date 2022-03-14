99e162502f6750240353457f9978e6ab818af8a7
ninyanin <91351206+ninyanin@users.noreply.github.com>
2022-03-14 15:43:12 -0400

    Add files via upload

---
+++ currency.machine.py
@@ -0,0 +1,32 @@
+def display_welcome_menu():
+    print("Welcome to the Orion IX COMP202COIN virtual exchange machine.")
+    print("Available options:")
+    print("1. Convert dollars into COMP202COIN")
+    print("2. Convert COMP202COIN into dollars")
+    print("3. Exit program")
+display_welcome_menu()
+
+x = input("Enter the amount of COMP202COIN to convert:")
+amount_of_comp202coin = (int(x, 16))
+
+SUN1_SET = false
+SUN2_SET = true
+SOLAR_OBSERVATION_FEE_MULTIPLIER = 0.05
+if SUN1_SET or SUN2_SET == true:
+    def get_solar_observation_fee(amount_of_comp202coin):
+        get_solar_observation_fee = amount_of_comp202coin * (SOLAR_OBSERVATION_FEE_MULTIPLIER*0)
+    
+    
+    
+    
+
+
+
+"""def operate_machine1(x):
+    print("Enter the amount of dollars to convert:")
+operate_machine1(1)
+def operate_machine2(x):
+    print("Enter the amount of COMP202COIN to convert:")
+operate_machine2(2)"""
+
+    
